fd0648079f8dc37a5d83a6c23af511772df05078
Dennis <d.lindo@gmail.com>
2022-05-13 01:08:46 -0400

    initial push

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,43 @@
 import React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-
+import '../styles/globals.css'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <StaticImage
-        src="../images/gatsby-astronaut.png"
-        alt="gatsby astronaut"
-      />
-    </div>
-    <p><Link to="/page-2/">Go to page 2</Link></p>
-    <p><Link to="/using-typescript/">Go to "Using TypeScript"</Link></p>
-  </Layout>
+<>
+<SEO title="Home" />
+    <section className="mt-0 h-[70vh] intro-title">      
+      <div className="line1 hover:bg-white"></div>
+      <div className="line2 hover:bg-white group">
+      <div className="line2a group-hover:bg-white"></div>
+      </div>
+
+      <div className="line3 hover:bg-white group">
+        <div className="line-top group-hover:bg-white"></div>
+      <div className="line4 group-hover:bg-white"></div>
+      </div>
+          
+      <div className="line5 hover:bg-white group">
+        <div className="line-top group-hover:bg-white"></div> 
+        <div className="line6 group-hover:bg-white">
+        <div className="line-bottom group-hover:bg-white"></div>
+      </div>       
+      </div>
+      
+      <div className="line7 group hover:bg-white">
+        <div className="line-top group-hover:bg-white"></div>
+        <div className="line8 group-hover:bg-white">
+        <div className="line-bottom group-hover:bg-white"></div>
+        </div>  
+      </div>
+      
+          
+
+      </section>
+      <h1 className="fade-in text-center py-5 font-extralight text-2xl tracking-wider">GRAPHIC DESIGNER AND MARKETING EXPERT</h1>
+  </>
 )
 
 export default IndexPage
