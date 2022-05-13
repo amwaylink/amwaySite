9e27e2edbeba24c890b942498936cd3385485dbe
Dennis <d.lindo@gmail.com>
2022-05-13 01:37:14 -0400

    gatsby clean

--- public/component---src-pages-page-2-tsx-f3d741efa43e13fb647d.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[473],{6567:function(e,t,a){a.r(t);var l=a(7294),n=a(1597),r=a(5592),c=a(262);t.default=function(e){return l.createElement(r.Z,null,l.createElement(c.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2 (",e.path,")"),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
-//# sourceMappingURL=component---src-pages-page-2-tsx-f3d741efa43e13fb647d.js.map
