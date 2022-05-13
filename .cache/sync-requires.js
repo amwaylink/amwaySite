754fdb08dbe3251fab9864b44d58fe25b5b18248
Dennis <d.lindo@gmail.com>
2022-05-13 01:23:16 -0400

    gatsby build push

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/dennislindo/Desktop/dennislindo.com/.cache/dev-404-page.js")),
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": preferDefault(require("/Users/dennislindo/Desktop/dennislindo.com/.cache/caches/gatsby-plugin-offline/app-shell.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/dennislindo/Desktop/dennislindo.com/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/dennislindo/Desktop/dennislindo.com/src/pages/index.js")),
   "component---src-pages-page-2-tsx": preferDefault(require("/Users/dennislindo/Desktop/dennislindo.com/src/pages/page-2.tsx")),
