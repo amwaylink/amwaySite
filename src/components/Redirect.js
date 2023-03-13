932aee1c79161391d107a96ddc72a81aae48568d
Dennis <d.lindo@gmail.com>
2023-03-13 15:20:00 -0400

    redirect no referrer

--- src/components/Redirect.js
+++ src/components/Redirect.js
@@ -3,6 +3,10 @@ import { useEffect } from "react"
 
 const Redirect = () => {
   useEffect(() => {
+    const meta = document.createElement("meta")
+    meta.name = "referrer"
+    meta.content = "no-referrer"
+    document.querySelector("head").appendChild(meta)
     window.location.href = "https://www.amway.com/share-link/kgFZNL1jM"
   }, [])
 
