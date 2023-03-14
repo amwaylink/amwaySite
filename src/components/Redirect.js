275c9ec26685623a7cdda4f02cfb8f63fe27ebb8
Dennis <dlindo@gmail.com>
2023-03-14 01:44:51 -0400

    3 second delay

--- src/components/Redirect.js
+++ src/components/Redirect.js
@@ -1,17 +1,35 @@
 import * as React from "react"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const Redirect = () => {
+  const [isLoading, setIsLoading] = useState(true)
+
   useEffect(() => {
     const meta = document.createElement("meta")
     meta.name = "referrer"
     meta.content = "no-referrer nofollow noopener"
     meta.description = "Amway Home Products Nutrilite"
     document.querySelector("head").appendChild(meta)
-    window.location.href = "https://www.amway.com/share-link/CewU4MDT3"
+    const timer = setTimeout(() => {
+      setIsLoading(false)
+      window.location.href = "https://www.amway.com/share-link/CewU4MDT3"
+    }, 3000) // 3 seconds delay
+    return () => clearTimeout(timer)
   }, [])
 
-  return null
+  if (isLoading) {
+    // Render a loading component while the delay is happening
+    return (
+      <div className="absolute inset-0 w-full h-full flex justify-center items-center bg-red-600">
+        <div className="flex flex-row justify-center items-center">
+          <div className="rounded-full w-4 h-4 bg-blue-800"></div>
+        </div>
+      </div>
+    )
+  } else {
+    // Replace the loading component with the redirect component once the delay is over
+    return null
+  }
 }
 
 export default Redirect
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import Redirect from "../components/Redirect"
 
 const IndexPage = () => {
-  return <Redirect />
+  return
+  ;<Redirect />
 }
 export default IndexPage
