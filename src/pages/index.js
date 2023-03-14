5bd25a4ed3c8aa182dba8b9c8d74f34239607406
amwaylink <127369652+amwaylink@users.noreply.github.com>
2023-03-14 11:59:19 -0400

    update redirect v2

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import * as React from "react"
 import Redirect from "../components/Redirect"
 
 const IndexPage = () => {
-  return
-  ;<Redirect />
+  return <Redirect />
 }
 export default IndexPage
