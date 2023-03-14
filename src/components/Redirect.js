ea40036bf9fa3db6e1c4fc141cdff58585f71324
Dennis <dlindo@gmail.com>
2023-03-13 20:32:24 -0400

    final redirect

--- src/components/Redirect.js
+++ src/components/Redirect.js
@@ -5,10 +5,10 @@ const Redirect = () => {
   useEffect(() => {
     const meta = document.createElement("meta")
     meta.name = "referrer"
-    meta.content = "no-referrer"
+    meta.content = "no-referrer nofollow noopener"
     meta.description = "Amway Home Products Nutrilite"
     document.querySelector("head").appendChild(meta)
-    window.location.href = "https://www.amway.com/share-link/kgFZNL1jM"
+    window.location.href = "https://www.amway.com/share-link/CewU4MDT3"
   }, [])
 
   return null
