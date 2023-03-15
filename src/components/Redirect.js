c64a1fc15176e2cf8e91adba05ea7f40e782dec7
Dennis <dlindo@gmail.com>
2023-03-15 01:05:18 -0400

    humberto shop

--- src/components/Redirect.js
+++ src/components/Redirect.js
@@ -8,7 +8,7 @@ const Redirect = () => {
     meta.content = "no-referrer nofollow noopener"
     meta.description = "Amway Home Products Nutrilite"
     document.querySelector("head").appendChild(meta)
-    window.location.href = "https://amwy.me/ne2TWbyHH"
+    window.location.href = "https://www.amway.com/en_US/myshop/vidal"
   }, [])
 
   return null
