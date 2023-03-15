5c772850b05dea8ad27daa5275c82d488855c187
Dennis <dlindo@gmail.com>
2023-03-14 21:53:48 -0400

    humberto short share link

--- src/components/Redirect.js
+++ src/components/Redirect.js
@@ -8,7 +8,7 @@ const Redirect = () => {
     meta.content = "no-referrer nofollow noopener"
     meta.description = "Amway Home Products Nutrilite"
     document.querySelector("head").appendChild(meta)
-    window.location.href = "https://www.amway.com/share-link/CewU4MDT3"
+    window.location.href = "https://amwy.me/ne2TWbyHH"
   }, [])
 
   return null
