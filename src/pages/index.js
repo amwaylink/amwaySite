fcb33cfd14e98c044fbd32c25e72220b4d9312f1
Dennis Lindo <d.lindo@gmail.com>
2024-05-15 14:06:19 -0400

    fresh start v2

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,21 +8,9 @@ import SEO from "../components/Seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <StaticImage
-        src="../images/gatsby-astronaut.png"
-        alt="gatsby astronaut"
-      />
+    <div>
+      <h1>Hi people</h1>
     </div>
-    <p>
-      <Link to="/page-2/">Go to page 2</Link>
-    </p>
-    <p>
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
   </Layout>
 )
 
