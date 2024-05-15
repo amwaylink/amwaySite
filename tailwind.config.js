50e79fe05c44a8dec2a59d9b34ac2b8915bf3814
Dennis Lindo <d.lindo@gmail.com>
2024-05-15 11:53:42 -0400

    cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", // Adjust this line to include all paths where TailwindCSS will be used
   ],
