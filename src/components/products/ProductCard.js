7ac50cd449351891f438b3977b8afeff3efe7e23
Dennis <dlindo@gmail.com>
2024-05-20 23:57:59 -0400

    product card fixed

--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -5,7 +5,11 @@ import React from "react"
 const ProductCard = ({ product }) => {
   const image = `https://www.amway.com${product.image_url}`
   return (
-    <button className="bg-white max-w-[22rem] rounded-xl shadow-lg grid grid-rows-[1fr_auto_autp] sm:grid-rows-[1fr_auto_1fr] w-full aspect-[3/5] sm:aspect-[3/4] overflow-hidden border my-5 group">
+    <a
+      href={product.link}
+      rel="noopener noreferrer"
+      className="bg-white max-w-[22rem] rounded-xl shadow-lg grid grid-rows-[1fr_auto_autp] sm:grid-rows-[1fr_auto_1fr] w-full aspect-[3/5] sm:aspect-[3/4] overflow-hidden border my-5 group"
+    >
       <img
         className="w-full aspect-square p-5"
         src={image}
@@ -16,14 +20,10 @@ const ProductCard = ({ product }) => {
         {product.product_title}
       </div>
 
-      <a
-        href={product.link}
-        rel="noopener noreferrer"
-        className="border-t h-full flex justify-center items-center group-hover:bg-gray-100 duration-300 ease-in-out font-bold"
-      >
+      <div className="border-t h-full flex justify-center items-center group-hover:bg-gray-100 duration-300 ease-in-out font-bold">
         View Product
-      </a>
-    </button>
+      </div>
+    </a>
   )
 }
 
