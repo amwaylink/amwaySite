08bd9ee1650529cf478edced1d8ee1dfd9b831ec
Dennis Lindo <d.lindo@gmail.com>
2024-08-07 14:45:13 -0400

    fixed gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,17 @@ module.exports = {
     THE_FLAG: false,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `AW-16585635220`,
+        // Puts tracking script in the head instead of the body
+        head: true,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
