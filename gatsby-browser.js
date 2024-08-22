8aae060ddb11b8d4068a095967a38c1f5012ebf1
Dennis Lindo <d.lindo@gmail.com>
2024-08-22 15:07:16 -0400

    added tracking

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,25 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
+import React from "react"
 import "./src/styles/globals.css"
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      async
+      key="google-gtag"
+      src="https://www.googletagmanager.com/gtag/js?id=AW-16585635220"
+    />,
+    <script
+      key="google-gtag-config"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-16585635220');
+        `,
+      }}
+    />,
+  ])
+}
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,25 @@
  */
 
 // You can delete this file if you're not using it
+import React from "react"
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      async
+      key="google-gtag"
+      src="https://www.googletagmanager.com/gtag/js?id=AW-16585635220"
+    />,
+    <script
+      key="google-gtag-config"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-16585635220');
+        `,
+      }}
+    />,
+  ])
+}
