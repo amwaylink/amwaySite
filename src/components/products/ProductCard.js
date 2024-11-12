3675e80131aeb70b0107ddd6338a6a0af26634e0
Dennis Lindo <d.lindo@gmail.com>
2024-11-12 18:12:16 -0500

    product card has gtag event

--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -1,14 +1,28 @@
-// src/components/ProductCard.js
-import { StaticImage } from "gatsby-plugin-image"
 import React from "react"
 
 const ProductCard = ({ product }) => {
   const image = `https://www.amway.com${product.image_url}`
+
+  // Handle product click for Google Ads tracking
+  const handleProductClick = () => {
+    if (window.gtag) {
+      window.gtag("event", "product_click", {
+        send_to: "AW-16585635220", // Replace with your Google Ads ID
+        event_category: "Product",
+        event_label: product.product_title,
+        value: 1,
+        product_category: product.category,
+        product_sku: product.sku,
+      })
+    }
+  }
+
   return (
     <a
       href={product.link}
       rel="noopener noreferrer"
-      className="bg-white max-w-[22rem] rounded-xl shadow-lg grid grid-rows-[1fr_auto_autp] sm:grid-rows-[1fr_auto_1fr] w-full aspect-[3/5] sm:aspect-[3/4] overflow-hidden border my-5 group"
+      className="bg-white max-w-[22rem] rounded-xl shadow-lg grid grid-rows-[1fr_auto_auto] sm:grid-rows-[1fr_auto_1fr] w-full aspect-[3/5] sm:aspect-[3/4] overflow-hidden border my-5 group"
+      onClick={handleProductClick} // Trigger tracking on click
     >
       <img
         className="w-full aspect-square p-5"
